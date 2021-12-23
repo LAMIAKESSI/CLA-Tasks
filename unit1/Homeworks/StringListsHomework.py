28db16a815de3358f583064c1bacfad255742979
your_username <hl_kessi@esi.dz>
2021-12-23 20:07:03 +0100

    Update String and Lists Homework

--- unit1/Homeworks/StringListsHomework.py
+++ unit1/Homeworks/StringListsHomework.py
@@ -1,3 +1,12 @@
 # 1 Create a list that contains all the even numbers between 1 and 299
+EvenNumbers = [n for n in range(1,300) if n % 2 == 0]
+print(EvenNumbers)
+    
 # 2 Iterate through the previously created list and print first the length of the list then all the squared values of the list
-# 3 In one line check if 57 is in the list using one line of python
+print(" The list length is :", len(EvenNumbers))
+for i in EvenNumbers:
+    print("The squared value of ", i, "is : ", i**2)
+
+# 3 check if 57 is in the list using one line 
+if (57 in EvenNumbers): print("57 exists in the list")
+else: print("57 does not exist in the list")
